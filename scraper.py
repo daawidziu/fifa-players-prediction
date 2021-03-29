64b6e2c87b7da3f3c5ad378c0be2df8d1584469e
daawidziu <daawidziu@int.pl>
2021-03-29 22:43:49 +0200

    Minor fixes, added time wait

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from typing import List
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def load_soup(data_url: str) -> BeautifulSoup:
@@ -52,18 +53,20 @@ def get_stats(data_url: str) -> dict:
     soup = load_soup(data_url)
     game_attributes = soup.find_all('span', class_='ig-stat-name-tooltip')
     game_values = soup.find_all('div', class_='stat_val', style=False)
-    personal_values = soup.find_all('td', class_='table-row-text', limit=9)
-    personal_attributes = soup.find_all('th', limit=9)
+    personal_values = soup.find_all('td', class_='table-row-text', limit=7)
+    personal_attributes = soup.find_all('th', limit=7)
     rating = soup.find('div', class_='pcdisplay-rat').get_text()
     game = {k.get_text(): v.get_text() for k, v in zip(game_attributes, game_values)}
     personal = {k.get_text(): v.get_text() for k, v in zip(personal_attributes, personal_values)}
-    statistic = {**game, **personal, 'rating': rating}
+    statistic = {**personal, **game, 'Rating': rating}
+    time.sleep(0.5)
     return statistic
 
 
 def get_links(pages: int) -> List:
     """
-    Paginate trough futbin.com to scrape links to player's pages. Search has been limited to only gold players.
+    Paginate trough futbin.com to scrape links to player's pages. Search has been limited to only gold players who
+    are not goalkeepers.
 
     Parameters:
         pages (int): number of pages to scrape
@@ -75,11 +78,12 @@ def get_links(pages: int) -> List:
     links = []
     for i in range(1, pages + 1):
         try:
-            r = requests.get('https://www.futbin.com/21/players?page={}&version=gold'.format(i))
+            r = requests.get('https://www.futbin.com/21/players?page={}&position=CB,LB,LWB,RB,RWB,CDM,CM,CAM,CF,ST,'
+                             'LM,LW,LF,RM,RW,RF&version=gold'.format(i))
             soup = BeautifulSoup(r.text, 'html.parser')
             for link in soup.find_all('a', {'class': 'player_name_players_table'}):
                 links.append(link.get('href'))
+            time.sleep(2)
         except requests.exceptions.HTTPError:
-            print('Only {} pages were found'.format(i))
-            break
+            pass
     return links
